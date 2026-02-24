bfed7a62826f4b2aa257cf3d73ce81de7efe5b3d
KARTHIK M <KARTHIK M@LAPTOP-FG6TU5Q7>
2026-02-24 15:31:07 +0530

    class1 modified

--- teky/src/test/java/teky/class1.java
+++ teky/src/test/java/teky/class1.java
@@ -1,11 +1,12 @@
 package teky;
 
-import java.security.PublicKey;
-
 public class class1 {
 
 	public static void main(String[] args) {
 		
-		Publ
-	}
+	System.out.println("hai karthik");
+
+	
 }
+}
+
